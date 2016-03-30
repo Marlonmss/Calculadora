5143a9a478271f60e36e8301ef3638da40c23ca2
Marlon <mss.904@gmail.com>
2016-03-29 22:55:00 -0300

    arquivos

--- Calculadora/Calculadora/Calculadora.mk
+++ Calculadora/Calculadora/Calculadora.mk
@@ -5,18 +5,18 @@
 ## Debug
 ProjectName            :=Calculadora
 ConfigurationName      :=Debug
-WorkspacePath          := "C:\Users\FACOMP\git\Calculadora\Calculadora"
-ProjectPath            := "C:\Users\FACOMP\git\Calculadora\Calculadora\Calculadora"
+WorkspacePath          := "C:\Users\Alguem\git\Calculadora\Calculadora"
+ProjectPath            := "C:\Users\Alguem\git\Calculadora\Calculadora\Calculadora"
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=FACOMP
-Date                   :=28/03/2016
+User                   :=Alguem
+Date                   :=29/03/2016
 CodeLitePath           :="C:\Program Files\CodeLite"
-LinkerName             :=C:/cygwin64/bin/x86_64-pc-cygwin-g++.exe
-SharedObjectLinkerName :=C:/cygwin64/bin/x86_64-pc-cygwin-g++.exe -shared -fPIC
+LinkerName             :="C:/Program Files (x86)/CodeBlocks/MinGW/bin/g++.exe"
+SharedObjectLinkerName :="C:/Program Files (x86)/CodeBlocks/MinGW/bin/g++.exe" -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
 PreprocessSuffix       :=.i
@@ -36,7 +36,7 @@ ObjectsFileList        :="Calculadora.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=makedir
 RcCmpOptions           := 
-RcCompilerName         :=windres
+RcCompilerName         :="C:/Program Files (x86)/CodeBlocks/MinGW/bin/windres.exe"
 LinkOptions            :=  
 IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
@@ -49,13 +49,13 @@ LibPath                := $(LibraryPathSwitch).
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := C:/cygwin64/bin/x86_64-pc-cygwin-ar.exe rcu
-CXX      := C:/cygwin64/bin/x86_64-pc-cygwin-g++.exe
-CC       := C:/cygwin64/bin/x86_64-pc-cygwin-gcc.exe
+AR       := "C:/Program Files (x86)/CodeBlocks/MinGW/bin/ar.exe" rcu
+CXX      := "C:/Program Files (x86)/CodeBlocks/MinGW/bin/g++.exe"
+CC       := "C:/Program Files (x86)/CodeBlocks/MinGW/bin/gcc.exe"
 CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
 CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
 ASFLAGS  := 
-AS       := C:/cygwin64/bin/x86_64-pc-cygwin-as.exe
+AS       := "C:/Program Files (x86)/CodeBlocks/MinGW/bin/as.exe"
 
 
 ##
@@ -94,7 +94,7 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/FACOMP/git/Calculadora/Calculadora/Calculadora/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Alguem/git/Calculadora/Calculadora/Calculadora/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM "main.cpp"
 
@@ -102,7 +102,7 @@ $(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) "main.cpp"
 
 $(IntermediateDirectory)/Calculadora.cpp$(ObjectSuffix): Calculadora.cpp $(IntermediateDirectory)/Calculadora.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/FACOMP/git/Calculadora/Calculadora/Calculadora/Calculadora.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Calculadora.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Alguem/git/Calculadora/Calculadora/Calculadora/Calculadora.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Calculadora.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Calculadora.cpp$(DependSuffix): Calculadora.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Calculadora.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Calculadora.cpp$(DependSuffix) -MM "Calculadora.cpp"
 
@@ -110,7 +110,7 @@ $(IntermediateDirectory)/Calculadora.cpp$(PreprocessSuffix): Calculadora.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Calculadora.cpp$(PreprocessSuffix) "Calculadora.cpp"
 
 $(IntermediateDirectory)/Data.cpp$(ObjectSuffix): Data.cpp $(IntermediateDirectory)/Data.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/FACOMP/git/Calculadora/Calculadora/Calculadora/Data.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Data.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Alguem/git/Calculadora/Calculadora/Calculadora/Data.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Data.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Data.cpp$(DependSuffix): Data.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Data.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Data.cpp$(DependSuffix) -MM "Data.cpp"
 
@@ -118,7 +118,7 @@ $(IntermediateDirectory)/Data.cpp$(PreprocessSuffix): Data.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Data.cpp$(PreprocessSuffix) "Data.cpp"
 
 $(IntermediateDirectory)/Ponto.cpp$(ObjectSuffix): Ponto.cpp $(IntermediateDirectory)/Ponto.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/FACOMP/git/Calculadora/Calculadora/Calculadora/Ponto.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Ponto.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Alguem/git/Calculadora/Calculadora/Calculadora/Ponto.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Ponto.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Ponto.cpp$(DependSuffix): Ponto.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Ponto.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Ponto.cpp$(DependSuffix) -MM "Ponto.cpp"
 
@@ -126,7 +126,7 @@ $(IntermediateDirectory)/Ponto.cpp$(PreprocessSuffix): Ponto.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Ponto.cpp$(PreprocessSuffix) "Ponto.cpp"
 
 $(IntermediateDirectory)/Memoria.cpp$(ObjectSuffix): Memoria.cpp $(IntermediateDirectory)/Memoria.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/FACOMP/git/Calculadora/Calculadora/Calculadora/Memoria.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Memoria.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Alguem/git/Calculadora/Calculadora/Calculadora/Memoria.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Memoria.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Memoria.cpp$(DependSuffix): Memoria.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Memoria.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Memoria.cpp$(DependSuffix) -MM "Memoria.cpp"
 
@@ -134,7 +134,7 @@ $(IntermediateDirectory)/Memoria.cpp$(PreprocessSuffix): Memoria.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Memoria.cpp$(PreprocessSuffix) "Memoria.cpp"
 
 $(IntermediateDirectory)/Cientifica.cpp$(ObjectSuffix): Cientifica.cpp $(IntermediateDirectory)/Cientifica.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/FACOMP/git/Calculadora/Calculadora/Calculadora/Cientifica.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Cientifica.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Alguem/git/Calculadora/Calculadora/Calculadora/Cientifica.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Cientifica.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Cientifica.cpp$(DependSuffix): Cientifica.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Cientifica.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Cientifica.cpp$(DependSuffix) -MM "Cientifica.cpp"
 
@@ -142,7 +142,7 @@ $(IntermediateDirectory)/Cientifica.cpp$(PreprocessSuffix): Cientifica.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Cientifica.cpp$(PreprocessSuffix) "Cientifica.cpp"
 
 $(IntermediateDirectory)/Financeira.cpp$(ObjectSuffix): Financeira.cpp $(IntermediateDirectory)/Financeira.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/FACOMP/git/Calculadora/Calculadora/Calculadora/Financeira.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Financeira.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Alguem/git/Calculadora/Calculadora/Calculadora/Financeira.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Financeira.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Financeira.cpp$(DependSuffix): Financeira.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Financeira.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Financeira.cpp$(DependSuffix) -MM "Financeira.cpp"
 
@@ -150,7 +150,7 @@ $(IntermediateDirectory)/Financeira.cpp$(PreprocessSuffix): Financeira.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Financeira.cpp$(PreprocessSuffix) "Financeira.cpp"
 
 $(IntermediateDirectory)/Dispositivo.cpp$(ObjectSuffix): Dispositivo.cpp $(IntermediateDirectory)/Dispositivo.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/FACOMP/git/Calculadora/Calculadora/Calculadora/Dispositivo.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Dispositivo.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Alguem/git/Calculadora/Calculadora/Calculadora/Dispositivo.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Dispositivo.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Dispositivo.cpp$(DependSuffix): Dispositivo.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Dispositivo.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Dispositivo.cpp$(DependSuffix) -MM "Dispositivo.cpp"
 
